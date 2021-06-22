544b925e76cb711c256b336c73b5f2dcdf1ff588
ys2005 <81169910+ys2005@users.noreply.github.com>
2021-06-22 14:32:59 -0400

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -6,12 +6,31 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(255); //background starts out white
 	frameRate(20);
+	rectMode(CENTER);
 }
+
+function keyPressed() {
+	if (keyCode == ENTER) { // checks whether the key that was pressed was the enter key
+		let rand = int(random(2));
+		let width = [10, 25, 50];
+		let height = [50, 80, 90];
+		fill(int(random(256)), int(random(256)), int(random(256)));
+		if (rand == 0) {
+			ellipse(int(random(windowWidth + 1)), int(random(windowHeight + 1)), random(width), random(height));
+		} else {
+			rect(int(random(windowWidth + 1)), int(random(windowHeight + 1)), random(width), random(height));
+		}
+	}
+	if (keyCode == 32) {
+		background(int(random(256)), int(random(256)), int(random(256)));
+	}
+}
+
 
 function draw() {
 	image(OMG, 0, 0, 3 / 4 * OMG.width, 3 / 4 * OMG.height); //GIF is in the top left corner of the screen, 3/4 of original size
 
-	print("Press down on your mouse and drag it around to see how the circles and squares follow it in a trail! To restart, click on any key, and the screen will be wiped with the background colour changed!")
+	print("Press down on your cursor and drag it around to see how the circles and squares follow it in a trail! To restart, click on the spacebar, and the screen will be wiped with the background colour changed! To see either a circle or rectangle placed upon the screen randomly, click on the enter key!")
 
 	textSize(25);
 	textAlign(RIGHT, BOTTOM); //text is in the bottom right of the screen
@@ -22,16 +41,16 @@ function draw() {
 		fill(int(random(256)), int(random(256)), int(random(256))); //shape colours are random
 		let x = [10, 20, 30, 40, 50]; //system randomly picks size within given options
 
-		stroke(128, 0, 128); //stroke colour is purple
-		strokeWeight(1);
-		ellipse(mouseX, mouseY, random(x));
+		if (mouseY <= windowHeight / 2) { // if the cursor is in the top half	
+			stroke(128, 0, 128); //stroke colour is purple
+			strokeWeight(1);
+			ellipse(mouseX, mouseY, random(x));
+		} else { // if the cursor is in the bottom half
+			stroke(0, 0, 255); //stroke colour is blue
+			strokeWeight(3);
+			rect(mouseX, mouseY, random(x));
+		}
 
-		stroke(0, 0, 255); //stroke colour is blue
-		strokeWeight(3);
-		rect(mouseX, mouseY, random(x)); //square is drawn because x is the same number 
 	}
 
-	if (keyIsPressed) {
-		background(int(random(256)), int(random(256)), int(random(256))); //if key is pressed, screen is wiped and background colour is random 
-	}
 }
